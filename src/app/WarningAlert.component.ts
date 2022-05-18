efd2cc264503e52de3e37176a1db2296c8fa069d
Abdelrhman-1998 <abdelrhman.ashraf.pro@gmail.com>
2022-05-18 05:46:30 +0200

    Assignment 1

---
+++ src/app/WarningAlert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './WarningAlert.component.html',
+  styleUrls: ['./WarningAlert.component.css']
+})
+export class WarningAlert{
+    
+}
